becbb080339b5721ad0ccb9b1ce31f9aabde1cf2
Yongwoo Park <yongwoo.park@42dot.ai>
2024-03-13 23:25:54 +0900

    add simple noise

--- samples/uv_panning.cpp
+++ samples/uv_panning.cpp
@@ -148,18 +148,86 @@ static Material *WindMaterial(Engine *engine) {
             }
         )SHADER")
         .material(R"SHADER(
+            float noise_randomValue (float2 uv) {
+                return fract(sin(dot(uv, float2(12.9898, 78.233)))*43758.5453);
+            }
+
+            float noise_interpolate (float a, float b, float t) {
+                return (1.0-t)*a + (t*b);
+            }
+
+            float valueNoise (float2 uv) {
+                float2 i = floor(uv);
+                float2 f = fract(uv);
+                f = f * f * (3.0 - 2.0 * f);
+
+                uv = abs(fract(uv) - 0.5);
+                float2 c0 = i + float2(0.0, 0.0);
+                float2 c1 = i + float2(1.0, 0.0);
+                float2 c2 = i + float2(0.0, 1.0);
+                float2 c3 = i + float2(1.0, 1.0);
+                float r0 = noise_randomValue(c0);
+                float r1 = noise_randomValue(c1);
+                float r2 = noise_randomValue(c2);
+                float r3 = noise_randomValue(c3);
+
+                float bottomOfGrid = noise_interpolate(r0, r1, f.x);
+                float topOfGrid = noise_interpolate(r2, r3, f.x);
+                float t = noise_interpolate(bottomOfGrid, topOfGrid, f.y);
+                return t;
+            }
+
+
+            void SimpleNoise_float(float2 UV, float Scale, out float Out)
+            {
+                float t = 0.0;
+
+                float freq = pow(2.0, float(0));
+                float amp = pow(0.5, float(3-0));
+                t += valueNoise(float2(UV.x*Scale/freq, UV.y*Scale/freq))*amp;
+
+                freq = pow(2.0, float(1));
+                amp = pow(0.5, float(3-1));
+                t += valueNoise(float2(UV.x*Scale/freq, UV.y*Scale/freq))*amp;
+
+                freq = pow(2.0, float(2));
+                amp = pow(0.5, float(3-2));
+                t += valueNoise(float2(UV.x*Scale/freq, UV.y*Scale/freq))*amp;
+
+                Out = t;
+            }
+
+            void tilingAndOffset(float2 UV, float2 Tiling, float2 Offset, out float2 Out) {
+                    Out = UV * Tiling + Offset;
+            }
+
+            float3 f_f3(float f) {
+                return fract(f / float3(16777216, 65536, 256)); 
+            }
+
+            void power(float a, float b, out float Out) {
+                Out = pow(a, b);
+            }
+
+
             void material(inout MaterialInputs material) {
                 prepareMaterial(material);
-                // float4 alpha = texture(materialParams_ALPHACOLOR_MAP, getUV0());
-                // float time = getUserTime().x;
-                // float2 panOffset = vec2(time * 0.45, time * 0.06);
-                // float2 uv = getUV0() - panOffset;
-                // float2 uv = getUV0();
-                // material.baseColor = texture(materialParams_BASECOLOR_MAP, uv);
-                // material.baseColor.rgb *= material.baseColor.a;
+                float2 uv = getUV0();
+                float nodeOut = 0.0;
 
+                float2 afterTilingAndOffset = float2(0.0, 0.0);
+                tilingAndOffset(uv,
+                    float2(3.0, 0.2),
+                    float2(0.0, 0.0),
+                    afterTilingAndOffset);
+
+                SimpleNoise_float(afterTilingAndOffset, 10.0, nodeOut);
+
+                power(nodeOut, 2.0, nodeOut);
+
+                material.baseColor.rgb = vec3(nodeOut, nodeOut, nodeOut);
                 // material.emissive = texture(materialParams_BASECOLOR_MAP, uv);
-                material.emissive = vec4(vec3(1.0, 1.0, 1.0), 0.0);
+                material.emissive = vec4(vec3(0.0), 0.0);
                 // material.emissive *= texture(materialParams_BASECOLOR_MAP, uv);
                 
             }
